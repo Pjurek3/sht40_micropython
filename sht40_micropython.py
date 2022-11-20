1522d71bfd86b47cbbb0a2498faabdfd8d8a0132
Paul Jurek <pjurek3@gmail.com>
2022-11-19 20:17:51 -0800

    initial commit

---
+++ sht40_micropython.py
@@ -0,0 +1,10 @@
+"""class interface to the sht40 using i2c
+
+this is based off the circuit python version 
+https://github.com/adafruit/Adafruit_CircuitPython_SHT4x"""
+
+import machine
+import struct
+import time
+
+
